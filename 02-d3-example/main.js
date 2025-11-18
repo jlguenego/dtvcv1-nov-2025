44adc3b5900e9fd6f8a43fae2d10a142e111e197
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-11-18 15:29:57 +0000

    Enhance SVG diagram by adding labels and adjusting rectangle widths based on river lengths

--- 02-d3-example/main.js
+++ 02-d3-example/main.js
@@ -24,6 +24,7 @@ const recupererDonnees = async () => {
     }
 
     const json = await response.json();
+    console.log('json: ', json);
 
     const data = json.results.bindings.map(b => ({
         name: b.riverLabel?.value || "(sans nom)",
@@ -38,8 +39,12 @@ const afficheDiagramme = (data) => {
     const svgns = "http://www.w3.org/2000/svg";
 
     const svg = document.querySelector("svg");
+    console.log('svg: ', svg);
     const height = 40;
     const gap = 10;
+    const width = +svg.getAttribute("width") - 2*10
+    const ratio =  width / data[0].km; 
+    console.log('ratio: ', ratio);
 
     for (let i = 0; i < 10; i++) {
         console.log(`Iteration number: ${i}`);
@@ -48,9 +53,18 @@ const afficheDiagramme = (data) => {
         const rect = document.createElementNS(svgns, "rect");
         rect.setAttribute("x", 10);
         rect.setAttribute("y", 10 + i * (height+gap));
-        rect.setAttribute("width", data[i].km);
+        rect.setAttribute("width", data[i].km * ratio);
         rect.setAttribute("height", height);
+
+        const text = document.createElementNS(svgns, "text");
+        text.setAttribute("x", 10 + 10);
+        text.setAttribute("y", 10 + i * (height+gap) + height / 2);
+        text.setAttribute("dominant-baseline", "middle");
+        text.innerHTML = `${data[i].name} (${data[i].km} km)`;
+        
         svg.appendChild(rect);
+        svg.appendChild(text);
+
     }
 }
 
