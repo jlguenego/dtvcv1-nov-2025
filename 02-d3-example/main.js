6edcf221b614bf50ebcd4f2fe5113a8f24d36055
Jean-Louis GUENEGO <jlguenego@gmail.com>
2025-11-19 09:37:59 +0000

    Update HTML and CSS for improved font loading and styling; enhance JavaScript data retrieval and visualization

--- 02-d3-example/main.js
+++ 02-d3-example/main.js
@@ -6,15 +6,16 @@ console.log('d3: ', d3);
 const recupererDonnees = async () => {
     const endpoint = "https://query.wikidata.org/sparql";
     const query = `
-      SELECT ?river ?riverLabel ?length
-      WHERE {
-        ?river wdt:P403 wd:Q1471 ;  # embouchure = Seine
-               wdt:P2043 ?length .  # longueur (en km)
-        SERVICE wikibase:label {
-          bd:serviceParam wikibase:language "fr,en".
-        }
-      }
-      ORDER BY DESC(?length)
+      SELECT ?river ?riverLabel ?length ?bassinVersant
+WHERE {
+  ?river wdt:P403 wd:Q1471 .  # embouchure = Seine
+  ?river wdt:P2043 ?length .  # longueur (en km)
+  ?river wdt:P2053 ?bassinVersant .  # Surface du bassin versant (en km2)
+  SERVICE wikibase:label {
+    bd:serviceParam wikibase:language "fr,en".
+  }
+}
+ORDER BY DESC(?length)
     `;
 
     const url = endpoint + "?query=" + encodeURIComponent(query);
@@ -31,7 +32,8 @@ const recupererDonnees = async () => {
 
     const data = json.results.bindings.map(b => ({
         name: b.riverLabel?.value || "(sans nom)",
-        km: parseFloat(b.length.value)
+        km: parseFloat(b.length.value),
+        surfaceBassinVersant: parseFloat(b.bassinVersant.value)
     }));
     console.log('data: ', data);
 
@@ -45,7 +47,7 @@ const afficheDiagramme = (data) => {
     console.log('svg: ', svg);
     const height = 40;
     const gap = 10;
-    const width = +svg.getAttribute("width") - 2*10
+    const width = +svg.getAttribute("width") - 2*10;
     const ratio =  width / data[0].km; 
     console.log('ratio: ', ratio);
 
@@ -72,12 +74,14 @@ const afficheDiagramme = (data) => {
 }
 
 const afficheDiagrammeWithD3 = (data) => {
+    const textMaxWidth = 70;
+    const kmTextWidth = 50;
     const svg = d3.select("svg");
     console.log('svg: ', svg);
     const height = 40;
     const gap = 10;
-    const width = +svg.attr("width") - 2*10
-    const ratio =  width / data[0].km; 
+    const maxBarWith = +svg.attr("width") - 2*10 - textMaxWidth - kmTextWidth;
+    const ratio =  maxBarWith / data[0].km; 
 
     const groupes = svg.selectAll("g")
         .data(data.slice(0,10))
@@ -86,19 +90,25 @@ const afficheDiagrammeWithD3 = (data) => {
         .attr("transform", (d,i) => `translate(0, ${10 + i * (height + gap)})`);
 
     groupes.append("rect")
-        .attr("x", 10)
+        .attr("x", 10 + textMaxWidth)
         .attr("width", d => d.km * ratio)
         .attr("height", height)
-        .attr("fill", "coral");
-        // .attr("fill", d => 
-        //     d3.interpolateViridis(d.km / data[0].km)
-        // );
+        .attr("fill", "coral")
+        .attr("fill", d => 
+            d3.interpolateViridis(d.km / data[0].km)
+        );
 
     groupes.append("text")
         .attr("x", 10 + 10)
         .attr("y", height / 2)
         .attr("dominant-baseline", "middle")
-        .text(d => `${d.name} (${d.km} km)`);
+        .text(d => `${d.name}`);
+
+    groupes.append("text")
+        .attr("x", d => d.km * ratio + 10 + textMaxWidth + 10)
+        .attr("y", height / 2)
+        .attr("dominant-baseline", "middle")
+        .text(d => `${d.km}`);
 }
 
 const main = async () => {
